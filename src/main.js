221a1a1866f2552c0d1dc955870c6237654b8ab9
Jasper <Jasper>
2020-05-22 10:14:04 +0800

    修改 百度统计

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ Vue.config.productionTip = false;
 import "iview/dist/styles/iview.css";
 
 import "@/assets/style/index.scss";
+
+var _hmt = _hmt || [];
+window._hmt = _hmt; // 必须把_hmt挂载到window下，否则找不到
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2ff41b2a46b0870d1efd2e6dc99a0395";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -16,84 +16,89 @@ Vue.use(Router);
 import Layout from "@/layout/Layout";
 
 export const constantRouterMap = [
-  {
-    path: "/",
-    component: Layout,
-    redirect: "home",
-    hidden: true,
-    children: [
-      {
-        path: "home",
-        component: () => import("@/views/home/index"),
-        name: "首页",
-        meta: { title: "首页", icon: "index" }
-      }
-    ]
-  },
-  {
-    path: "/article",
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: "",
-        component: () => import("@/views/article/index"),
-        name: "文章",
-        meta: { title: "文章", icon: "index" }
-      }
-    ]
-  },
-  {
-    path: "/tools",
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: "",
-        component: () => import("@/views/tools/index"),
-        name: "工具",
-        meta: { title: "工具", icon: "index" }
-      }
-    ]
-  }
-  // {
-  //   path: "/404",
-  //   component: () => import("@/views/errorPage/404"),
-  //   hidden: true
-  // },
-  // {
-  //   path: "/401",
-  //   component: () => import("@/views/errorPage/401"),
-  //   hidden: true
-  // }
+    {
+        path: "/",
+        component: Layout,
+        redirect: "home",
+        hidden: true,
+        children: [
+            {
+                path: "home",
+                component: () => import("@/views/home/index"),
+                name: "首页",
+                meta: { title: "首页", icon: "index" },
+            },
+        ],
+    },
+    {
+        path: "/article",
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: "",
+                component: () => import("@/views/article/index"),
+                name: "文章",
+                meta: { title: "文章", icon: "index" },
+            },
+        ],
+    },
+    {
+        path: "/tools",
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: "",
+                component: () => import("@/views/tools/index"),
+                name: "工具",
+                meta: { title: "工具", icon: "index" },
+            },
+        ],
+    },
+    // {
+    //   path: "/404",
+    //   component: () => import("@/views/errorPage/404"),
+    //   hidden: true
+    // },
+    // {
+    //   path: "/401",
+    //   component: () => import("@/views/errorPage/401"),
+    //   hidden: true
+    // }
 ];
 const router = new Router({
-  mode: "history",
-  // scrollBehavior: () => ({ y: 0 }),
-  routes: constantRouterMap,
-  scrollBehavior(to) {
-    if (to.hash) {
-      return {
-        selector: to.hash
-      };
-    }
-  }
+    mode: "history",
+    // scrollBehavior: () => ({ y: 0 }),
+    routes: constantRouterMap,
+    scrollBehavior(to) {
+        if (to.hash) {
+            return {
+                selector: to.hash,
+            };
+        }
+    },
 });
 
 import NProgress from "nprogress"; // progress bar
 import "nprogress/nprogress.css"; // progress bar style
 
 NProgress.configure({
-  showSpinner: false
+    showSpinner: false,
 }); // NProgress Configuration
 router.beforeEach((to, from, next) => {
-  NProgress.start();
-  // 跳转路由 把移动端的导航初始化
-  store.dispatch("getMobileOpenNav", false);
-  next();
+    NProgress.start();
+    // 跳转路由 把移动端的导航初始化
+    store.dispatch("getMobileOpenNav", false);
+    if (_hmt) {
+        if (to.path) {
+            _hmt.push(["_trackPageview", "/#" + to.fullPath]);
+        }
+    }
+    next();
 });
 
 router.afterEach(() => {
-  NProgress.done(); // finish progress bar
+    NProgress.done(); // finish progress bar
 });
 export default router;
